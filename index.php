3ad17e1fb464abd573b778e4d92d863a3a62c4df
Chris Yang <cyang287@yahoo.com>
2019-04-12 15:31:42 -0400

    finished html markup

---
+++ index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>Pontiac, MI, EDC</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,700i|Raleway:500,600,700,900|Roboto:700">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<header>
+	<a href="#main" id="skip" tabindex="0">SKIP TO CONTENT</a>
+
+	<div class="container">
+		<a href="./" id="logo" class="clearfix">
+			<img src="./_assets_/images/logo.png" alt="Logo">
+		</a><!-- /.logo -->
+
+		<div id="toggles" class="hidden-lg hidden-md">
+			<div tabindex="0" id="search-toggle-mobile" class="fa fa-search"></div>
+			<div tabindex="0" id="nav-toggle" class="fa fa-bars"></div>	
+		</div><!-- /#toggles -->
+
+		<div class="social-links hidden-sm hidden-xs">
+			<a href="./" class="fa fa-facebook"></a>
+			<a href="./" class="fa fa-twitter"></a>
+			<a href="./" class="fa fa-instagram"></a>
+			<a href="./" class="fa fa-youtube"></a>
+			<span tabindex="0" id="search-toggle" class="fa fa-search"></span>
+		</div><!-- /.social-links -->
+
+		<nav>
+			<ul id="nav">
+				<li><a href="./">home</a></li>
+				<li>
+					<a href="./">site selector</a>
+					<ul>
+						<li>
+							<span>section header</span>
+							<ul>
+								<li><a href="./">sem egestas mollis porta fusce</a></li>
+								<li><a href="./">adipiscing purus ultricies</a></li>
+								<li><a href="./">condimentum mollis elit</a></li>
+								<li><a href="./">euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing sem egestas mollis</a></li>
+								<li><a href="./">porta fusce adipiscing purus</a></li>
+								<li><a href="./">ultricies condimentum mattis</a></li>
+								<li><a href="./">elit euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing</a></li>
+							</ul>
+						</li>
+						<li>
+							<span>section header</span>
+							<ul>
+								<li><a href="./">sem egestas mollis porta fusce</a></li>
+								<li><a href="./">adipiscing purus ultricies</a></li>
+								<li><a href="./">condimentum mollis elit</a></li>
+								<li><a href="./">euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing sem egestas mollis</a></li>
+								<li><a href="./">porta fusce adipiscing purus</a></li>
+								<li><a href="./">ultricies condimentum mattis</a></li>
+								<li><a href="./">elit euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing</a></li>
+							</ul>
+						</li>
+						<li>
+							<span>section header</span>
+							<ul>
+								<li><a href="./">sem egestas mollis porta fusce</a></li>
+								<li><a href="./">adipiscing purus ultricies</a></li>
+								<li><a href="./">condimentum mollis elit</a></li>
+								<li><a href="./">euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing sem egestas mollis</a></li>
+								<li><a href="./">porta fusce adipiscing purus</a></li>
+								<li><a href="./">ultricies condimentum mattis</a></li>
+								<li><a href="./">elit euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing</a></li>
+							</ul>
+						</li>
+						<li>
+							<span>section header</span>
+							<ul>
+								<li><a href="./">sem egestas mollis porta fusce</a></li>
+								<li><a href="./">adipiscing purus ultricies</a></li>
+								<li><a href="./">condimentum mollis elit</a></li>
+								<li><a href="./">euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing sem egestas mollis</a></li>
+								<li><a href="./">porta fusce adipiscing purus</a></li>
+								<li><a href="./">ultricies condimentum mattis</a></li>
+								<li><a href="./">elit euismod egestas mattis</a></li>
+								<li><a href="./">adipiscing</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="./">incentives</a></li>
+				<li><a href="./">businesses</a></li>
+				<li><a href="./">get started</a></li>
+			</ul> <!-- /#nav -->
+		</nav>
+
+		<div id="search">
+			<form class="search-form" method="get" action="search.php">
+				<label class="fa fa-search" for="search-input"></label>
+				<input name="q" class="form-control search-input" placeholder="Enter search terms..." type="search" id="search-input">
+				<button><i class="fa fa-search"></i></button>
+			</form><!-- /.search-form -->
+		</div><!-- /#search -->
+	</div><!-- /.container -->
+</header><!-- /.clearfix -->
+
+<section id="slider">
+	<ul class="bxslider">
+		<li style="background: url('./_assets_/images/hero1.jpg') center no-repeat;background-size:cover;"></li>
+	</ul><!-- /.bxslider -->
+</section><!-- /#slider -->
+
+<main id="main">
+	<section id="quick-links">
+		<div class="container">
+			<div id="quick-links-wrapper" class="owl-carousel">
+				<a href="./" class="quick-link">
+					<div class="quick-link-icon" style="background:url('./_assets_/images/ql1.png') center no-repeat;background-size:cover;"></div>
+					<h3>available properties</h3>
+				</a><!-- /.quick-links -->
+				<a href="./" class="quick-link">
+					<div class="quick-link-icon" style="background:url('./_assets_/images/ql2.png') center no-repeat;background-size:cover;"></div>
+					<h3>gis maps</h3>
+				</a><!-- /.quick-links -->
+				<a href="./" class="quick-link">
+					<div class="quick-link-icon" style="background:url('./_assets_/images/ql3.png') center no-repeat;background-size:cover;"></div>
+					<h3>application process</h3>
+				</a><!-- /.quick-links -->
+				<a href="./" class="quick-link">
+					<div class="quick-link-icon" style="background:url('./_assets_/images/ql4.png') center no-repeat;background-size:cover;"></div>
+					<h3>frequently asked questions</h3>
+				</a><!-- /.quick-links -->
+			</div><!-- /#quick-links-wrapper.owl-carousel -->
+		</div><!-- /.container -->
+	</section><!-- /#quick-links -->
+
+	<section id="content">
+		<section id="why">
+			<div id="why-bg" style="background:url('./_assets_/images/why-bg.jpg') center no-repeat;background-size:cover;"></div>
+			<div class="container">
+				<h2>why pontiac</h2>
+				<div id="why-info-wrapper">
+					<div class="why-info-container">
+						<div class="why-info">
+							<h4>Population of 59,000</h4>
+							<span>Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit.</span>
+						</div><!-- /.why-info -->
+						<div class="why-info-icon" style="background:url('./_assets_/images/why1.png') center no-repeat;background-size:cover;"></div>
+						<h3>demographics</h3>
+					</div><!-- /.why-info-container -->
+					<div class="why-info-container">
+						<div class="why-info">
+							<h4>Population of 59,000</h4>
+							<span>Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit.</span>
+						</div><!-- /.why-info -->
+						<div class="why-info-icon" style="background:url('./_assets_/images/why2.png') center no-repeat;background-size:cover;"></div>
+						<h3>housing</h3>
+					</div><!-- /.why-info-container -->
+					<div class="why-info-container">
+						<div class="why-info">
+							<h4>Population of 59,000</h4>
+							<span>Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit.</span>
+						</div><!-- /.why-info -->
+						<div class="why-info-icon" style="background:url('./_assets_/images/why3.png') center no-repeat;background-size:cover;"></div>
+						<h3>retailers</h3>
+					</div><!-- /.why-info-container -->
+					<div class="why-info-container">
+						<div class="why-info">
+							<h4>Population of 59,000</h4>
+							<span>Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit.</span>
+						</div><!-- /.why-info -->
+						<div class="why-info-icon" style="background:url('./_assets_/images/why4.png') center no-repeat;background-size:cover;"></div>
+						<h3>starting a business</h3>
+					</div><!-- /.why-info-container -->
+					<div class="why-info-container">
+						<div class="why-info">
+							<h4>Population of 59,000</h4>
+							<span>Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit.</span>
+						</div><!-- /.why-info -->
+						<div class="why-info-icon" style="background:url('./_assets_/images/why5.png') center no-repeat;background-size:cover;"></div>
+						<h3>properties</h3>
+					</div><!-- /.why-info-container -->
+				</div><!-- /#why-links -->
+			</div><!-- /.container -->
+		</section><!-- /#why -->
+
+		<section id="useful-links">
+			<div id="useful-links-wrapper" class="owl-carousel">
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful1.png') center no-repeat;background-size:cover;"></div>
+					<h3>available resources</h3>
+				</a><!-- /.useful=link -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful2.png') center no-repeat;background-size:cover;"></div>
+					<h3>economic data</h3>
+				</a><!-- /.useful=link -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful3.png') center no-repeat;background-size:cover;"></div>
+					<h3>find a site</h3>
+				</a><!-- /.useful=link -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful4.png') center no-repeat;background-size:cover;"></div>
+					<h3>top industries</h3>
+				</a><!-- /.useful=link -->
+				<a href="./" class="useful-link">
+					<div class="useful-link-icon" style="background:url('./_assets_/images/useful5.png') center no-repeat;background-size:cover;"></div>
+					<h3>quality of life</h3>
+				</a><!-- /.useful=link -->
+			</div><!-- /#useful-links-wrapper -->
+		</section><!-- /#useful-links -->
+
+		<section id="news-events-local">
+			<div class="container">
+				<div class="row">
+					<section id="news" class="col-md-4">
+						<div class="cleafix">
+							<h2>news</h2>
+							<a href="./" class="btn">see all news</a>
+						</div><!-- /.clearfix -->
+						<div id="news-links" class="owl-carousel">
+							<div class="news-link-wrapper">
+								<a class="news-link" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;">
+									<div class="news-link-info">
+										<span>march 31st, 2017</span>
+										<h3>news post title cursus dapibus ipsum sit inceptos</h3>
+									</div><!-- /.news-link-info -->
+								</a><!-- /.news-link -->
+								<a class="news-link" style="background:url('./_assets_/images/news2.jpg') center no-repeat;background-size:cover;">
+									<div class="news-link-info">
+										<span>april 9th, 2017</span>
+										<h3>tristique ligula quam dolor ridiculus</h3>
+									</div><!-- /.news-link-info -->
+								</a><!-- /.news-link -->
+							</div><!-- /.news-links-wrapper -->
+						</div><!-- /#news-links -->
+					</section><!-- /#news -->
+
+					<section id="events" class="col-md-5">
+						<div class="clearfix">
+							<h2>events</h2>
+							<a href="./" class="btn">see all events</a>
+						</div><!-- /.clearfix -->
+						<div><img src="./_assets_/images/events.png" alt="Events"></div>
+					</section><!-- /#events -->
+
+					<section id="local-business" class="col-md-3" style="background:url('./_assets_/images/local-business.jpg') center no-repeat;background-size:cover;">
+						<div id="local-business-icon" style="background:url('./_assets_/images/local-business-icon.png') center no-repeat;background-size:cover;"></div>
+						<h3>local business directory</h3>
+						<p>
+							View all of the business Pontiac vhas to offer. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.
+						</p>
+						<a href="./" class="btn">view directory</a>
+					</section>
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /#news-events -->
+	</section><!-- /#content -->
+</main>
+
+<footer>
+	<section id="revize-logo">
+		<div class="container">
+			<span>&copy; Pontiac, MI Economic Development</span>
+			<span>Powered by <a href="https://www.revize.com/">revize</a>, The Government Website Experts. <a href="./">login</a></span>
+		</div><!-- /.container -->
+	</section><!-- /#revize-logo -->
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4>Alert Title 30 characters Max</h4>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
+<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
